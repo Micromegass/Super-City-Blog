75b166c1c47367ff7de1a3f5236fd7ebcafb85aa
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-06-09 17:18:43 -0500

    added some styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -57,7 +57,7 @@ before_action :private_access, except: [:index, :show]
 
     private
     def post_params
-        params.require(:post).permit(:title, :description, :id)
+        params.require(:post).permit(:title, :description, :id, :image)
     end 
 
 end
