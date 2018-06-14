f7bf776638f011a52f094821d85296e79d58f330
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-06-14 17:45:45 -0500

    added gem for dates

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Superblog
     config.i18n.default_locale = :es
     config.i18n.enforce_available_locales = true
 
+    config.i18n.available_locales = ['es-CO', :es]
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
