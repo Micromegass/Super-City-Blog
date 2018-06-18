4d9e5b9d97c40e8869f9c6c89f4bfcf683c162a8
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-06-17 20:08:28 -0500

    added url

--- app/models/post.rb
+++ app/models/post.rb
@@ -24,7 +24,7 @@ class Post < ApplicationRecord
   validates :description, presence: true,  length: { minimum: 250 }
 
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", :url  => ":s3_domain_url",
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
