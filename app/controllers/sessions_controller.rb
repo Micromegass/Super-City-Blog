a18ab72f1c3c04f5fe964914ce3fc98abf923c7e
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-07-05 12:39:35 +0200

    added default background image

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,23 +2,19 @@ class SessionsController < ApplicationController
   before_action :private_access, only: [:destroy]
   before_action :public_access, except: [:destroy]
   
- 
   def create
-      user = User.find_by(email: params[:email])
+    user = User.find_by(email: params[:email])
       if user && user.authenticate(params[:password])
         sign_in(user)
        redirect_to root_path
       else
         flash[:alert] = "Tu email y/o contraseña son inválidas. Intenta nuevamente."
         render :new
-   end
+      end
   end
-
-
 
   def destroy  
     sign_out
     redirect_to root_path
   end 
-
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -8,8 +8,8 @@ module PostsHelper
      if @post.image.exists?
     %{ style="background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2) ), url('#{@post.image.url}'); height:100%; background-position:center; background-repeat:no-repeat; background-size:cover" }.html_safe
       
-   else
-    ""
+    else
+    %{ style="background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2) ), url('https://images.pexels.com/photos/37728/pexels-photo-37728.jpeg?cs=srgb&dl=above-atmosphere-clouds-37728.jpg&fm=jpg'); height:100%; background-position:center; background-repeat:no-repeat; background-size:cover" }.html_safe
+    end 
   end 
-  end 
-  end 
+end 
